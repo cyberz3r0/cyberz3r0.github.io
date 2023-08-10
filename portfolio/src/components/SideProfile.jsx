ecd854e67e682689fc6f54dde9874bbd1c98aa0a
cyberz3r0 <reidsl0716@gmail.com>
2023-08-09 19:07:29 -0700

    add image

--- portfolio/src/components/SideProfile.jsx
+++ portfolio/src/components/SideProfile.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Sam from "../assets/Sam.jpg"
 
 const SideProfile = () => {
   return (
-    <div className="w-auto bg-red-800 rounded-xl">
+    <div className="w-auto bg-[#1a1918] rounded-xl mt-24">
+        <img src={Sam} alt="Sam" className="rounded-xl -mt-5 w-2/3 m-auto relative bottom-24"/>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi consectetur, similique temporibus eveniet esse ut quidem omnis, ab vero laboriosam dolorem, possimus pariatur eius mollitia ratione error doloremque? Nihil, cupiditate.
         Omnis tempore, quam voluptatibus quis explicabo eaque harum maxime corporis iure reprehenderit sed, neque consectetur minus enim ratione placeat quibusdam saepe. Id consectetur consequuntur, officia ullam delectus molestias explicabo nostrum.
         Impedit voluptatibus sint excepturi error voluptates! Temporibus voluptatibus, dolorum quis eum error perferendis nisi deserunt accusantium debitis omnis laboriosam ullam molestiae expedita aut veritatis dolore eius repellat quidem sit nesciunt.</p>
