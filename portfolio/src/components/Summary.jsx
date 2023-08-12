c1372e0ce4e39a9615d60b0df3eac56985aab4d8
cyberz3r0 <reidsl0716@gmail.com>
2023-08-11 21:36:55 -0700

    design tweak

--- portfolio/src/components/Summary.jsx
+++ portfolio/src/components/Summary.jsx
@@ -14,6 +14,7 @@ const Summary = () => {
                 <Nav />
                 <div className="p-3 bg-[#3b3938] rounded-xl">
                     <p className="text-4xl text-white font-bold">About Me</p>
+                    <hr className="w-1/2" />
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, provident, sit sapiente neque aspernatur ullam deserunt ea vero fuga saepe magnam aliquam consequatur in earum. Nulla officiis distinctio dolore excepturi!
                     Rem similique eum necessitatibus, dolorem, blanditiis fuga accusamus voluptates facere ab fugiat id ad deleniti fugit accusantium saepe nemo mollitia eius delectus dignissimos enim doloribus aliquam itaque. Adipisci, corrupti nulla?
                     Voluptatum ex neque dolore doloribus similique totam quasi iste optio reprehenderit, rem, sequi facere. Cum incidunt tenetur ullam temporibus repellendus enim eum consequuntur accusantium ex repudiandae. Delectus quo dignissimos non!
