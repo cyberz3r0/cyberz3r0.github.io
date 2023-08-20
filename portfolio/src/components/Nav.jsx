a64e26f06c38678bfcb7b279166674372900c48a
cyberz3r0 <reidsl0716@gmail.com>
2023-08-19 22:37:16 -0700

    added about me and skills

--- portfolio/src/components/Nav.jsx
+++ portfolio/src/components/Nav.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import { FaHouseChimney, FaFileLines, FaGear } from "react-icons/fa6";
+import { FaHouseChimney, FaFileLines, FaGear, FaBars } from "react-icons/fa6";
 
 const Nav = () => {
   let iconStyle = { color: "#1a1918", fontSize: "1.5em", margin: "0 auto" };
   let classStyles= "h-16 bg-[#a5a8a7] text-[#1a1918] w-3/12 border-1 rounded-lg p-3 font-medium"
   return (
     <>
-      
-      <nav class="container flex justify-end bg-white">
-        <div class="hidden space-x-8 lg:flex justify-end mb-8 bg-[#3b3938] p-5 rounded-xl w-96">
+    <div className="hidden md:flex justify-end mb-8">
+      <div className=" h-26 rounded-xl p-5 bg-[#3b3938] w-96 flex justify-evenly">
           <div className={classStyles}>
             <FaHouseChimney style={iconStyle}/>
             <p className="text-center">Home</p>
@@ -22,15 +21,12 @@ const Nav = () => {
             <p className="text-center">Projects</p>
           </div>
         </div>
-        <div class="flex lg:hidden">
-          <div class="space-y-2">
-            <span class="block w-8 h-0.5 bg-gray-600 animate-pulse"></span>
-            <span class="block w-8 h-0.5 bg-gray-600 animate-pulse"></span>
-            <span class="block w-8 h-0.5 bg-gray-600 animate-pulse"></span>
-          </div>
-        </div>
-      </nav>
-      
+    </div>
+    <div className="md:hidden">
+      <div className="">
+      <FaBars style={{color:"black", fontSize:"48px", margin:"0 0 10px auto", backgroundColor:"#a5a8a7", borderRadius:"10px", padding:"5px"}}/>
+      </div>
+    </div>
     </>
   )
 }
--- portfolio/src/components/Summary.jsx
+++ portfolio/src/components/Summary.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import Nav from './Nav'
 import SideProfile from './SideProfile'
-
+import {BiLogoFlask, BiLogoDjango,BiLogoPython,BiLogoJavascript,BiLogoMongodb, BiLogoNodejs,BiLogoAws,BiLogoReact} from 'react-icons/bi'
+import {SiMysql} from 'react-icons/si'
 const Summary = () => {
+
+    let icons={fontSize:"48px"}
 
   return (
     <div>
@@ -13,14 +16,35 @@ const Summary = () => {
             <div className="max-w-screen-lg">
                 <Nav />
                 <div className="p-3 bg-[#3b3938] rounded-xl">
-                    <p className="text-4xl text-white font-bold">About Me</p>
-                    <hr className="w-1/2" />
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, provident, sit sapiente neque aspernatur ullam deserunt ea vero fuga saepe magnam aliquam consequatur in earum. Nulla officiis distinctio dolore excepturi!
-                    Rem similique eum necessitatibus, dolorem, blanditiis fuga accusamus voluptates facere ab fugiat id ad deleniti fugit accusantium saepe nemo mollitia eius delectus dignissimos enim doloribus aliquam itaque. Adipisci, corrupti nulla?
-                    Voluptatum ex neque dolore doloribus similique totam quasi iste optio reprehenderit, rem, sequi facere. Cum incidunt tenetur ullam temporibus repellendus enim eum consequuntur accusantium ex repudiandae. Delectus quo dignissimos non!
-                    Minima officia et nostrum eos eligendi nihil quas perspiciatis harum inventore beatae deleniti obcaecati aliquid, quam qui rerum quos accusamus sequi quia blanditiis error laboriosam, voluptatum distinctio voluptates? Sed, nostrum!
-                    Dolor, quam, ipsam distinctio dolore repellat eum molestiae eos officia esse animi necessitatibus hic debitis quod, quos ut fugiat maiores reiciendis quibusdam. Est ea placeat cumque deserunt fugit autem. Facilis?</p>
-                
+                    <p className="text-4xl text-white font-bold m-5">About Me</p>
+                    <hr className="w-1/2 mx-5" />
+                    <p className="text-black bg-[#a5a8a7] rounded-xl p-5 m-5">Motivated individual seeking a junior developer role with a company that will benefit from my technical knowledge in the IT field. I'm a uncut gem ready to be molded to shape. <br /><br />
+
+                    My background is in customer service and technical support. I used to work in a high volume and fast-paced environment where I have collaborated with clients and team members in small teams. <br /> <br />
+
+                    My interest in programming actually started in college and I tried to purse self-teaching until I finally enrolled in Coding Dojo due to the COVID pandemic. I quickly found myself fascinated with coding and helping others understand what we learned that I ended up being a TA for the same bootcamp. Outside of coding, I like to play games, go on hikes, and watching movies. <br /><br />
+
+                    I am in the market for a job as a developer. Feel free to reach out to me to network or if you want to chat.</p>
+                </div>
+                <div className="p-3 bg-[#3b3938] rounded-xl mt-5">
+                    <p className="text-4xl text-white font-bold m-5">Skills</p>
+                    <hr className="w-1/2 mx-5" />
+                    <div className="flex justify-between p-5 ">
+                        <BiLogoFlask style={icons}/>
+                        <BiLogoDjango style={icons}/>
+                        <BiLogoPython style={icons}/>
+                        <BiLogoJavascript style={icons}/>
+                        <BiLogoMongodb style={icons}/>
+                        <SiMysql style={icons}/>
+                        <BiLogoNodejs style={icons}/>
+                        <BiLogoReact style={icons}/>
+                        <BiLogoAws style={icons}/>
+
+
+
+
+
+                    </div>
                 </div>
             </div>
         </div>
