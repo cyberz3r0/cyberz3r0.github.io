fc318e0522b595cde2334f9350a66720b70777e4
cyberz3r0 <reidsl0716@gmail.com>
2023-10-16 21:20:38 -0700

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./cyberz3r0.github.io",
+  base: "/cyberz3r0.github.io/",
   plugins: [react()],
 })
